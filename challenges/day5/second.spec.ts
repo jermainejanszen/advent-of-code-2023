0df39c7d75bb408d8f5b2e305975af17b4da58cb
Jermaine Janszen <jermaine.janszen@wisetechglobal.com>
2023-12-06 11:41:28 +1100

    Finished day 5

---
+++ challenges/day5/second.spec.ts
@@ -0,0 +1,76 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+import { it } from "vitest";
+
+type SeedMap = {
+  seeds: number[];
+  chain: MapChain;
+};
+
+type MapChain = ((source: number) => number)[];
+
+const parseInput = (input: string) => {
+  const lines = input.split("\n").map((line) => line.trim());
+
+  const result: SeedMap = {
+    seeds: [],
+    chain: [],
+  };
+
+  result.seeds = lines[0]
+    .split(":")[1]
+    .trim()
+    .split(" ")
+    .map((value) => Number.parseInt(value.trim()))
+    .filter((value) => !Number.isNaN(value));
+
+  for (let i = 1; i < lines.length; i++) {
+    if (!lines[i]) {
+      const mapValues: number[][] = [];
+      for (let j = i + 2; j < lines.length; j++) {
+        if (!lines[j]) break;
+        mapValues.push(
+          lines[j].split(" ").map((value) => Number.parseInt(value.trim()))
+        );
+      }
+
+      const map = (value: number) => {
+        for (const mapping of mapValues) {
+          if (value >= mapping[1] && value < mapping[1] + mapping[2]) {
+            return mapping[0] + value - mapping[1];
+          }
+        }
+        return value;
+      };
+
+      result.chain.push(map);
+    }
+  }
+
+  return result;
+};
+
+const processSeed = (seed: number, chain: MapChain) => {
+  let result = seed;
+  for (const map of chain) {
+    result = map(result);
+  }
+  return result;
+};
+
+it.todo("is the solution to day 5 challenge 2", async () => {
+  const input = await readFile(join(__dirname, "input.txt"), "utf-8");
+  const map = parseInput(input);
+
+  let min = -1;
+  for (let i = 0; i < map.seeds.length; i += 2) {
+    for (let j = map.seeds[i]; j < map.seeds[i] + map.seeds[i + 1]; j++) {
+      const location = processSeed(j, map.chain);
+      if (min < 0 || location < min) {
+        min = location;
+      }
+    }
+  }
+
+  console.log("Day 5 Challenge 2:", min);
+});
